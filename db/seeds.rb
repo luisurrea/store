aa956f10209d50feece3fbb919547d0dfbc40512
Luis <luanur@hotmail.com>
2012-11-14 00:33:05 -0500

    nbm

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create(title: 'Programming Ruby 1.9',
+  description:
+    %{<p>
+      Ruby is the fastest growing and most exciting dynamic language
+      out there. If you need to get working programs delivered fast,
+      you should add ruby to your toolbox.
+      </p> },
+      image_url: 'rails.png',
+      price: 49.95)
